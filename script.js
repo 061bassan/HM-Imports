d26b1e02aa6f3ff6edbaf9d14c9c37ad76d24e83
Rafael Bassan <devbassanlopes@gmail.com>
2026-03-17 14:21:41 -0300

    Corrige links invisíveis sem JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+document.documentElement.classList.remove('no-js');
+document.documentElement.classList.add('js');
+
 document.addEventListener('DOMContentLoaded', () => {
   const links = document.querySelectorAll('.links a');
-  
+
   links.forEach((link, index) => {
     setTimeout(() => {
       link.classList.add('animate-in');
